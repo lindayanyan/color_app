59d82ba370f99acfabbdec6a5a80c3305d4f232c
lindayanyan <54968671+lindayanyan@users.noreply.github.com>
2020-12-06 18:53:08 -0800

    new features

--- app/src/main/java/com/example/colorapp/ColorPickerDialog.kt
+++ app/src/main/java/com/example/colorapp/ColorPickerDialog.kt
@@ -17,19 +17,54 @@
 
 package com.example.colorapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.skydoves.colorpickerview.ColorEnvelope
 import com.skydoves.colorpickerview.ColorPickerView
+import com.skydoves.colorpickerview.flag.BubbleFlag
+import com.skydoves.colorpickerview.flag.FlagMode
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 import com.skydoves.colorpickerview.sliders.AlphaSlideBar
 import com.skydoves.colorpickerview.sliders.BrightnessSlideBar
+
 
 /**
  * ColorPickerDialog is a dialog what having [ColorPickerView], [AlphaSlideBar] and
  * [BrightnessSlideBar].
  */
 class ColorPickerDialog: AppCompatActivity() {
+    //private var colorListener: ColorPickerViewListener? = null
+    private var colorPickerView: ColorPickerView? = null
+    private val TAG = photo.javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_picker_dialog2)
+
+        colorPickerView?.setHsvPaletteDrawable()
+        Log.d(TAG, "FILE2")
+
+        colorPickerView = findViewById(R.id.colorPickerView)
+        val bubbleFlag = BubbleFlag(this)
+        bubbleFlag.flagMode = FlagMode.FADE
+
+    //   colorPickerView.setFlagView(bubbleFlag)
+     //   colorPickerView.setColorListener(
+            ColorEnvelopeListener { envelope: ColorEnvelope, fromUser: Boolean ->
+                //Timber.d("color: %s", envelope.hexCode)
+                setLayoutColor(envelope)
+            }
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun setLayoutColor(envelope: ColorEnvelope) {
+        //val textView = findViewById<TextView>(R.id.textView)
+        //textView.text = "#" + envelope.hexCode
+       // val alphaTileView = findViewById<AlphaTileView>(R.id.alphaTileView)
+        //alphaTileView.setPaintColor(envelope.color)
+    }
+
+
 }
--- app/src/main/java/com/example/colorapp/MainActivity.kt
+++ app/src/main/java/com/example/colorapp/MainActivity.kt
@@ -5,16 +5,39 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.skydoves.colorpickerview.ColorEnvelope
+import com.skydoves.colorpickerview.ColorPickerView
+import com.skydoves.colorpickerview.flag.BubbleFlag
+import com.skydoves.colorpickerview.flag.FlagMode
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val colorPickerView: ColorPickerView? = findViewById(R.id.colorPickerView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
         // Get a support ActionBar corresponding to this toolbar and enable the Up button
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        colorPickerView?.setHsvPaletteDrawable()
+        //Log.d(TAG, "FILE2")
+
+        //colorPickerView = findViewById(R.id.colorPickerView)
+        val bubbleFlag = BubbleFlag(this)
+        bubbleFlag.flagMode = FlagMode.FADE
+
+        if (colorPickerView != null) {
+            colorPickerView.setFlagView(bubbleFlag)
+            colorPickerView.setColorListener(
+                ColorEnvelopeListener { envelope: ColorEnvelope, fromUser: Boolean ->
+                    //Timber.d("color: %s", envelope.hexCode)
+                   // setLayoutColor(envelope)
+                })
+        }
+
 
     }
 
@@ -28,22 +51,22 @@ class MainActivity : AppCompatActivity() {
 
     //code below is adapted from https://developer.android.com/training/appbar/actions
    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-       R.id.action_settings -> {
-           // User chose the "Settings" item, show the app settings UI...
-           true
-       }
+        R.id.action_settings -> {
+            // User chose the "Settings" item, show the app settings UI...
+            true
+        }
 
-       R.id.action_menu -> {
-           val toast = Toast.makeText(applicationContext, "Test",Toast.LENGTH_SHORT)
-           toast.show()
-           true
-       }
+        R.id.action_menu -> {
+            val toast = Toast.makeText(applicationContext, "Test", Toast.LENGTH_SHORT)
+            toast.show()
+            true
+        }
 
-       R.id.action_test -> {
-           val toast = Toast.makeText(applicationContext, "Test",Toast.LENGTH_SHORT)
-           toast.show()
-           true
-       }
+        R.id.action_test -> {
+            val toast = Toast.makeText(applicationContext, "Test", Toast.LENGTH_SHORT)
+            toast.show()
+            true
+        }
 
 
        else -> {
@@ -52,6 +75,13 @@ class MainActivity : AppCompatActivity() {
            super.onOptionsItemSelected(item)
        }
    }
+
+    /*private fun setLayoutColor(envelope: ColorEnvelope) {
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "#" + envelope.hexCode
+        val alphaTileView = findViewById<AlphaTileView>(R.id.alphaTileView)
+        alphaTileView.setPaintColor(envelope.color)
+    }*/
 
 
 }
